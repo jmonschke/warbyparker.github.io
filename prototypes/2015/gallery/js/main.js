188bfa0c780b9f781776a94752e202726f54069c
Pon <kattera@gmail.com>
2015-03-25 14:56:04 -0400

    Gallery Exploration

---
+++ prototypes/2015/gallery/js/main.js
@@ -0,0 +1,132 @@
+
+
+var waypoint = new Waypoint({
+  element: document.getElementById('headturn'),
+  handler: function(direction) {
+    $( "#headturn" ).css( "left", "-1600px" );
+  },
+  offset: '50%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('headturn'),
+  handler: function(direction) {
+    $( "#headturn" ).css( "left", "-800px" );
+  },
+  offset: '35%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('headturn'),
+  handler: function(direction) {
+    $( "#headturn" ).css( "left", "0px" );
+  },
+  offset: '20%'
+})
+
+
+// var waypoint = new Waypoint({
+//   element: document.getElementById('glasses3'),
+//   handler: function(direction) {
+//     $( "#glasses3" ).toggleClass("slide-up");
+//     $( "#glasses4" ).toggleClass("slide-up");
+//   },
+//   offset: '90%'
+// })
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup1'),
+  handler: function(direction) {
+    $( ".glasses-slideup1" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup2'),
+  handler: function(direction) {
+    $( ".glasses-slideup2" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup3'),
+  handler: function(direction) {
+    $( ".glasses-slideup3" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup4'),
+  handler: function(direction) {
+    $( ".glasses-slideup4" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup5'),
+  handler: function(direction) {
+    $( ".glasses-slideup5" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup6'),
+  handler: function(direction) {
+    $( ".glasses-slideup6" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup7'),
+  handler: function(direction) {
+    $( ".glasses-slideup7" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup8'),
+  handler: function(direction) {
+    $( ".glasses-slideup8" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup9'),
+  handler: function(direction) {
+    $( ".glasses-slideup9" ).toggleClass("slide-up");
+  },
+  offset: '93%'
+})
+
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup10'),
+  handler: function(direction) {
+    $( ".glasses-slideup10" ).toggleClass("slide-up");
+  },
+  offset: '86%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup11'),
+  handler: function(direction) {
+    $( ".glasses-slideup11" ).toggleClass("slide-up");
+  },
+  offset: '86%'
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementsByClassName('glasses-slideup12'),
+  handler: function(direction) {
+    $( ".glasses-slideup12" ).toggleClass("slide-up");
+  },
+  offset: '86%'
+})
+
+
